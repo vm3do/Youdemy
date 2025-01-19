c327d69d3416d630c538815f234851b7bd7c1108
vm3do <medoavvadi@gmail.com>
2025-01-19 21:51:22 +0100

    tags logic

---
+++ actions/adminFunc.php
@@ -0,0 +1,23 @@
+<?php
+
+    require_once "../Config/Database.php";
+    require "../Classes/Tags.php";
+    require "../Classes/Category.php";
+
+    $db = new Database();
+    $pdo = $db->conn();
+
+    $tag_err = "";
+    $cat_err = "";
+
+    if(isset($_POST['add_tags'])){
+
+        $Tags = new Tags($pdo);
+        $return = $Tags->addTags($_POST['tags']) ;
+
+        $tag_err = $return["message"];
+    }
+
+    if(isset($_POST['add_categories'])){
+
+    }
